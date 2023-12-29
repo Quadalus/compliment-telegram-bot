cd250e72e4e3393ce544984cecb1184a3afb0646
quadalus <quadalus@yandex.ru>
2023-12-29 04:23:26 +0300

    feat

--- telegram-bot/src/main/java/ru/bikkul/compliment/telegram/bot/service/BotService.java
+++ telegram-bot/src/main/java/ru/bikkul/compliment/telegram/bot/service/BotService.java
@@ -1,10 +1,6 @@
 package ru.bikkul.compliment.telegram.bot.service;
 
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-import java.io.File;
-import java.util.concurrent.CompletableFuture;
 
 @EnableAsync
 public interface BotService {
